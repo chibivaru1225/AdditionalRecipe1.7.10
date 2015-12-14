8b432319f76c9f9e95296f8d51412dc7e8915e63
chibivaru <chibivaru@live.jp>
2015-12-14 23:13:34 +0900

    ちょっとづつソースコードを追加

--- additionalrecipe/AdditionalRecipe.java
+++ additionalrecipe/AdditionalRecipe.java
@@ -2,24 +2,30 @@ package chibivaru.additionalrecipe;
 
 import java.util.HashMap;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import chibivaru.additionalrecipe.core.ModInfo;
+import chibivaru.additionalrecipe.tool.CheaperExchangeIgnition;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Metadata;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(
-		modid   = AdditionalRecipe.MODID,
-		name    = AdditionalRecipe.MODNAME,
-		version = AdditionalRecipe.VERSION,
-		dependencies="after:AppliedEnergistics;"
+		modid        = AdditionalRecipe.MODID,
+		name         = AdditionalRecipe.MODNAME,
+		version      = AdditionalRecipe.VERSION,
+		dependencies = "after:AppliedEnergistics;"
 				+ "after:MineFactoryReloaded;"
 				+ "after:ThermalExpansion;"
 				+ "after:BuildCraftCore;"
 				+ "after:Railcraft;"
-				+ "after:DCsAppleMilk"
+				+ "after:DCsAppleMilk;"
+				+ "required-after:FML"
 	)
 
 public class AdditionalRecipe
@@ -33,9 +39,25 @@ public class AdditionalRecipe
 
 	HashMap<String,Item> ARItems;
 
-	@EventHandler
-	public void init(FMLInitializationEvent event)
+	//@Mod.Instance("AdditionalRecipe")
+	//public static AdditionalRecipe instance;
+	public static CheaperExchangeIgnition cheaperExchangeIgnition;
+
+	@Mod.EventHandler
+	public void preinit(FMLPreInitializationEvent event)
 	{
 		ModInfo.loadInfo(meta);
+
+		cheaperExchangeIgnition = (CheaperExchangeIgnition)(new CheaperExchangeIgnition()).setUnlocalizedName("cheaperexchangeignition").setCreativeTab(CreativeTabs.tabTools);
+
+		GameRegistry.registerItem(cheaperExchangeIgnition, "CheaperExchangeIgnition");
+	}
+
+	@Mod.EventHandler
+	public void load(FMLInitializationEvent event)
+	{
+		FMLCommonHandler.instance().bus().register(cheaperExchangeIgnition);
+
+		LanguageRegistry.addName(cheaperExchangeIgnition, "CheaperExchangeIgnition");
 	}
 }
---
+++ additionalrecipe/tool/CheaperExchangeIgnition.java
@@ -0,0 +1,63 @@
+package chibivaru.additionalrecipe.tool;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import chibivaru.additionalrecipe.AdditionalRecipe;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class CheaperExchangeIgnition extends Item
+{
+	private boolean repair;
+	private boolean effect;
+
+	public CheaperExchangeIgnition()
+	{
+		super();
+		this.setMaxDamage(128);
+		this.setMaxStackSize(1);
+	}
+
+	public boolean doesContainerItemLeaveCraftingGrid(ItemStack par1ItemStack)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean hasContainerItem()
+	{
+		return !repair;
+	}
+
+	@SubscribeEvent
+	public void onCrafting(ItemCraftedEvent event)
+	{
+		//IDが無くなったので、アイテムインスタンスで比較。
+		repair = AdditionalRecipe.cheaperExchangeIgnition == event.crafting.getItem();
+	}
+
+	@Override
+	public ItemStack getContainerItem(ItemStack itemStack)
+	{
+		if (itemStack != null && itemStack.getItem() == this)
+		{
+			itemStack.setItemDamage(itemStack.getItemDamage() + 1);
+		}
+		return itemStack;
+	}
+
+	public int getColorFromItemStack(ItemStack par1ItemStack, int par2)
+	{
+		return 0x666666;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister)
+	{
+		this.itemIcon = Items.diamond.getIconFromDamage(0);
+	}
+}
