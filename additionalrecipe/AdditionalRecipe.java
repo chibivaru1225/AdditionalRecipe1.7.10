1f7fb7dcdc490959fea506815c943cd7210a1521
chibivaru <chibivaru@live.jp>
2022-06-07 08:47:12 +0900

    バージョン修正

--- additionalrecipe/AdditionalRecipe.java
+++ additionalrecipe/AdditionalRecipe.java
@@ -48,7 +48,7 @@ public class AdditionalRecipe
 {
     public static final String             MODID         = "additionalrecipe";
     public static final String             MODNAME       = "AdditionalRecipe";
-    public static final String             VERSION       = "0.02";
+    public static final String             VERSION       = "0.01-18";
 
     public static final String             CONSOLE       = "[AdditionalRecipe]:";
     public static final String             ADDID         = " added ID ";
